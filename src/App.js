ea18bada8f09a1db81842a1713d794150baa0414
Corentin Deleuse <dlse.corentin@gmail.com>
2023-05-16 18:18:59 +0200

    setup connection socket

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import io from 'socket.io-client'
+import { useEffect } from 'react';
+const socket = io.connect('http://localhost:3001')
 
 function App() {
+
+  useEffect(()=>{
+    socket.on('receive_message', (data)=> {
+      alert(data.message)
+    })
+
+  }, [socket])
+
+  const sendMessage = () => {
+      socket.emit("send_message", {message: "hello"})
+  }
+
   return (
     <div className="App">
-      
+        <input type="text" placeholder="Message"/>
+        <button onClick={sendMessage}>Send</button>
     </div>
   );
 }
