fc14508cbcf9ef4aa37d8cce73d3a96a93c433cb
Corentin Deleuse <dlse.corentin@gmail.com>
2023-05-18 16:44:00 +0200

    save user pseudo

--- server/index.js
+++ server/index.js
@@ -1,25 +1,60 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').createServer(app);
+const server = require("http").createServer(app);
 const { Server } = require("socket.io");
+const fs = require("fs");
 
 // socket.io
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-        allowedHeaders: ["my-custom-header"],
-        credentials: true
-    }
+  cors: {
+    origin: "http://192.168.1.6:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
 });
 
-io.on('connection', (socket)=> {
-  console.log(`User Connected: ${socket.id}`);
-  socket.on("send_message", (data) => {
-    socket.broadcast.emit('receive_message', data )
-  })
-})
+io.on("connection", (socket) => {
+  console.log('a user connected');
 
-server.listen(3001, ()=>{
-  console.log('SERVER IS RUNNING');
-})
+  socket.on("save_pseudo", (pseudo) => {
+    
+    socket.pseudo = pseudo.userPseudo;
+
+    // Lire le contenu du fichier JSON existant
+    fs.readFile("./data/users.json", "utf-8", (err, data) => {
+      if (err) {
+        console.error("Erreur lors de la lecture du fichier JSON :", err);
+        return;
+      }
+
+      // Analyser le contenu JSON existant en un objet JavaScript
+      let users = JSON.parse(data);
+      // Ajouter le nouveau pseudo à l'objet JavaScript
+      users[socket.id] = { pseudo: socket.pseudo };
+      // Convertir l'objet JavaScript en chaîne de caractères JSON
+      const jsonContent = JSON.stringify(users);
+
+      // Enregistrer le contenu mis à jour dans le fichier
+      fs.writeFile("./data/users.json", jsonContent, "utf8", (err) => {
+        if (err) {
+          console.error(
+            "Erreur lors de l'enregistrement du fichier JSON :",
+            err
+          );
+          return;
+        }
+        console.log("Pseudo ajouté avec succès dans le fichier JSON.");
+      });
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+
+server.listen(3001, () => {
+  console.log("SERVER IS RUNNING");
+});
--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
-import './App.css';
-import io from 'socket.io-client'
-import { useEffect, useState } from 'react';
-const socket = io.connect('http://localhost:3001')
+import "./App.css";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+
+const socket = io.connect("http://192.168.1.6:3001");
 
 function App() {
+  const [userPseudo, setUserPseudo] = useState("");
+  const [error, setError] = useState("");
 
-  const [message, setMessage] = useState("");
-  const [messageReceived, setMessageReceived] = useState("");
-  const sendMessage = () => {
-      socket.emit("send_message", { message })
-  }
+  const savePseudo = () => {
+    if(userPseudo !== "" && userPseudo) {
+      socket.emit("save_pseudo", { userPseudo });
+      setError("")
+    } else {
+      setError("Veuillez rentrer un pseudo")
+    }
+  };
 
-  useEffect(()=>{
-    socket.on('receive_message', (data)=> {
-      setMessageReceived(data.message)
-    })
+  socket.on("disconnect", () => {
+    socket.emit("disconnected", true); 
+  });
 
-  }, [socket])
+  useEffect(() => {
+    socket.on("receive_message", (data) => {});
+  }, []);
 
   return (
-    <div className="App">
-        <input type="text" placeholder="Message" onChange={(event) => setMessage(event.target.value)}/>
-        <button onClick={sendMessage}>Send</button>
-        <h2>Messages:</h2>
-        {messageReceived}
+    <div className="w-full h-screen bg-neutral-900 flex justify-center items-center">
+      <div className=" px-20 py-16 bg-neutral-800 rounded-lg">
+        <label for="pseudo" class=" cursor-pointer block mb-2 text-base font-medium text-neutral-900 dark:text-white">Your Pseudo</label>
+        <input
+          type="text"
+          name="pseudo"
+          id="pseudo"
+          maxLength={12}
+          className="bg-neutral-50 border border-neutral-300 text-neutral-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-neutral-700 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          onChange={(event) => setUserPseudo(event.target.value)}
+        />
+        {error && <p className="text-sm text-rose-900 mt-1">{error}</p>}
+        <button onClick={savePseudo} className="bg-white px-4 py-1 rounded hover:bg-neutral-200 mt-8 m-auto">Save</button>
+      </div>
     </div>
   );
 }
