f84e112fdedf5e06243f5de59e3fcd34f740def8
Corentin Deleuse <dlse.corentin@gmail.com>
2023-05-22 16:37:47 +0200

    reset message input

--- src/App.js
+++ src/App.js
@@ -59,6 +59,7 @@ function App() {
 
   const sendMessage = () => {
     socket.emit("send_message", newMessage);
+    setNewMessage("")
     setInfosSave(true);
   };
 
@@ -81,9 +82,9 @@ function App() {
   }, [messages]);
 
   return infosSave ? (
-    <div className="h-screen flex relative bg-neutral-900">
-      <div className="h-full w-60 py-4 px-4 ">
-        <div className="h-full bg-neutral-800 rounded-lg">{userPseudo}</div>
+    <div className="h-screen flex relative bg-neutral-950">
+      <div className="h-full w-60 py-4 px-4">
+        <div className="h-full bg-neutral-900 rounded-lg flex justify-center  py-4 px-4 text-white">{userPseudo}</div>
       </div>
 
       <div className="flex-1 h-full ">
@@ -99,11 +100,15 @@ function App() {
             const minutes = date.getMinutes();
             const seconds = date.getSeconds();
             return (
-              <div className={`flex flex-col my-4 ${additionalClass}`} key={data.id}>
-                <div className={`text-white ${additionalClass}`}>{data.pseudo}</div>
+              <div
+                className={`flex flex-col my-4 ${additionalClass}`}
+                key={data.id}
+              >
+                <div className={`text-white ${additionalClass}`}>
+                  {data.pseudo}
+                </div>
                 <div
                   className={`flex flex-col bg-neutral-500 py-2 px-4 rounded-lg  `}
-                  
                 >
                   {data.message}
                   <span className="text-xs text-neutral-700">{`${year}-${month}-${day} ${hours}:${minutes}:${seconds}`}</span>
@@ -117,8 +122,9 @@ function App() {
             type="text"
             name="chat"
             id="chat"
+            value={newMessage}
             onChange={(event) => setNewMessage(event.target.value)}
-            className="w-full max-w-2xl bg-neutral-50 border border-neutral-300 text-neutral-900 text-sm rounded-l-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-neutral-700 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="w-full max-w-2xl bg-neutral-50 border border-neutral-300 text-neutral-950 text-sm rounded-l-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-neutral-700 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
           <button
             onClick={sendMessage}
@@ -130,7 +136,7 @@ function App() {
       </div>
 
       <div className=" w-60 py-4 px-4 ">
-        <div className="px-8 py-4 h-full bg-neutral-800 rounded-lg">
+        <div className="px-8 py-4 h-full bg-neutral-900 rounded-lg">
           <ul>
             {usersList.map((data) => {
               return (
@@ -151,12 +157,12 @@ function App() {
       </div>
     </div>
   ) : (
-    <div className="w-full h-screen bg-neutral-900 flex justify-center items-center">
-      <div className=" px-20 py-16 bg-neutral-800 rounded-lg flex flex-col ">
+    <div className="w-full h-screen bg-neutral-950 flex justify-center items-center">
+      <div className=" px-20 py-16 bg-neutral-900 rounded-lg flex flex-col ">
         <div>
           <label
             htmlFor="pseudo"
-            className="cursor-pointer mb-2 text-base font-medium text-neutral-900 dark:text-white"
+            className="cursor-pointer text-base font-medium text-neutral-900 dark:text-white"
           >
             Ton pseudo
           </label>
@@ -165,29 +171,45 @@ function App() {
             name="pseudo"
             id="pseudo"
             maxLength={12}
-            className="bg-neutral-50 border border-neutral-300 text-neutral-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-neutral-700 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-neutral-50 border mt-2 border-neutral-300 text-neutral-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-neutral-700 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             onChange={(event) => setUserPseudo(event.target.value)}
           />
         </div>
 
-        <div onChange={(event) => setUserGender(event.target.value)}>
+        <div
+          className="py-4"
+          onChange={(event) => setUserGender(event.target.value)}
+        >
           <input type="radio" name="genre" id="male" value={"Male"} />
           <label
-            className="cursor-pointer mb-2 text-base font-medium text-neutral-900 dark:text-white"
+            className="cursor-pointer ml-2 text-base font-medium text-neutral-900 dark:text-white"
             htmlFor="male"
           >
             Homme
           </label>
-          <input type="radio" name="genre" id="female" value={"Female"} />
+          <input
+            className="ml-4"
+            type="radio"
+            name="genre"
+            id="female"
+            value={"Female"}
+          />
           <label
-            className="cursor-pointer mb-2 text-base font-medium text-neutral-900 dark:text-white"
+            className="cursor-pointer ml-2 text-base font-medium text-neutral-900 dark:text-white"
             htmlFor="female"
           >
             Femme
           </label>
         </div>
 
-        <div>
+        <div className="flex items-center">
+          <label
+            htmlFor="age"
+            className="mr-2  cursor-pointer mb-2 text-base font-medium text-neutral-900 dark:text-white"
+          >
+            Ton âge
+          </label>
+
           <input
             className=" text-sm rounded-lg block p-2.5 bg-neutral-700 border-neutral-600 placeholder-neutral-400 text-white focus:ring-blue-500 focus:border-blue-500"
             onChange={(event) => setUserAge(event.target.value)}
@@ -200,7 +222,7 @@ function App() {
           />
         </div>
 
-        {error && <p className="text-sm text-rose-900 mt-1">{error}</p>}
+        <p className="text-sm text-rose-900 h4 mt-1">{error}</p>
         <button
           onClick={saveUserInfos}
           className="bg-white px-4 py-1 rounded hover:bg-neutral-200 mt-8 m-auto"
